e33e6ea43832c5ebee33a812ee2627d8f8cd6e43
Kévin Dunglas <dunglas@gmail.com>
2020-01-28 11:42:08 +0100

    net/http/httputil: add an option to overwrite forwarded headers in ReverseProxy

--- src/net/http/httputil/reverseproxy_test.go
+++ src/net/http/httputil/reverseproxy_test.go
@@ -236,7 +236,48 @@ func TestReverseProxyStripHeadersPresentInConnection(t *testing.T) {
 	}
 }
 
-func TestDontTrustForwardedHeaders(t *testing.T) {
+func TestXForwardedFor(t *testing.T) {
+	const prevForwardedFor = "client ip"
+	const backendResponse = "I am the backend"
+	const backendStatus = 404
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-Forwarded-For") == "" {
+			t.Errorf("didn't get X-Forwarded-For header")
+		}
+		if !strings.Contains(r.Header.Get("X-Forwarded-For"), prevForwardedFor) {
+			t.Errorf("X-Forwarded-For didn't contain prior data")
+		}
+		w.WriteHeader(backendStatus)
+		w.Write([]byte(backendResponse))
+	}))
+	defer backend.Close()
+	backendURL, err := url.Parse(backend.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	proxyHandler := NewSingleHostReverseProxy(backendURL)
+	frontend := httptest.NewServer(proxyHandler)
+	defer frontend.Close()
+
+	getReq, _ := http.NewRequest("GET", frontend.URL, nil)
+	getReq.Host = "some-name"
+	getReq.Header.Set("Connection", "close")
+	getReq.Header.Set("X-Forwarded-For", prevForwardedFor)
+	getReq.Close = true
+	res, err := frontend.Client().Do(getReq)
+	if err != nil {
+		t.Fatalf("Get: %v", err)
+	}
+	if g, e := res.StatusCode, backendStatus; g != e {
+		t.Errorf("got res.StatusCode %d; expected %d", g, e)
+	}
+	bodyBytes, _ := ioutil.ReadAll(res.Body)
+	if g, e := string(bodyBytes), backendResponse; g != e {
+		t.Errorf("got body %q; expected %q", g, e)
+	}
+}
+
+func TestOverwriteForwardedHeaders(t *testing.T) {
 	const prevForwardedFor = "client ip"
 	const prevForwardedProto = "https"
 	const prevForwardedHost = "example.com"
@@ -264,58 +305,7 @@ func TestDontTrustForwardedHeaders(t *testing.T) {
 		t.Fatal(err)
 	}
 	proxyHandler := NewSingleHostReverseProxy(backendURL)
-	frontend := httptest.NewServer(proxyHandler)
-	defer frontend.Close()
-
-	getReq, _ := http.NewRequest("GET", frontend.URL, nil)
-	getReq.Host = "some-name"
-	getReq.Header.Set("Connection", "close")
-	getReq.Header.Set("X-Forwarded-For", prevForwardedFor)
-	getReq.Header.Set("X-Forwarded-Proto", prevForwardedProto)
-	getReq.Header.Set("X-Forwarded-Host", prevForwardedHost)
-	getReq.Close = true
-	res, err := frontend.Client().Do(getReq)
-	if err != nil {
-		t.Fatalf("Get: %v", err)
-	}
-	if g, e := res.StatusCode, backendStatus; g != e {
-		t.Errorf("got res.StatusCode %d; expected %d", g, e)
-	}
-	bodyBytes, _ := ioutil.ReadAll(res.Body)
-	if g, e := string(bodyBytes), backendResponse; g != e {
-		t.Errorf("got body %q; expected %q", g, e)
-	}
-}
-
-func TestXForwardedFor(t *testing.T) {
-	const prevForwardedFor = "client ip"
-	const prevForwardedProto = "https"
-	const prevForwardedHost = "example.com"
-	const backendResponse = "I am the backend"
-	const backendStatus = 404
-	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("X-Forwarded-For") == "" {
-			t.Errorf("didn't get X-Forwarded-For header")
-		}
-		if !strings.Contains(r.Header.Get("X-Forwarded-For"), prevForwardedFor) {
-			t.Errorf("X-Forwarded-For didn't contain prior data")
-		}
-		if !strings.Contains(r.Header.Get("X-Forwarded-Proto"), prevForwardedProto) {
-			t.Errorf("X-Forwarded-Proto didn't contain prior data")
-		}
-		if !strings.Contains(r.Header.Get("X-Forwarded-Host"), prevForwardedHost) {
-			t.Errorf("X-Forwarded-Host didn't contain prior data")
-		}
-		w.WriteHeader(backendStatus)
-		w.Write([]byte(backendResponse))
-	}))
-	defer backend.Close()
-	backendURL, err := url.Parse(backend.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-	proxyHandler := NewSingleHostReverseProxy(backendURL)
-	proxyHandler.TrustForwardedHeaders = true
+	proxyHandler.OverwriteForwardedHeaders = true
 	frontend := httptest.NewServer(proxyHandler)
 	defer frontend.Close()
 
